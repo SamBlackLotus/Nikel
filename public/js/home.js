9c24980757579c324c28d2e1628c024e4329e0bb
SamBlackLotus <sam.blacklotus@gmail.com>
2022-12-20 01:51:42 -0300

    Mostrando as entradas e saídas

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,11 @@
 const myModal = new bootstrap.Modal("#transaction-modal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
+let data = {
+    transactions: []
+};
 
+document.getElementById("button-logout").addEventListener("click", logout);
 
 checkLogged();
 
@@ -11,10 +15,50 @@ function checkLogged() {
         logged = session;
     }
 
-    if (logged) {
-        saveSession(logged, session);
-        window.location.href = "home.html"
-
+    if (!logged) {
+        window.location.href = "index.html";
+        return;
     }
 
+    const dataUser = localStorage.getItem(logged);
+    if(dataUser) {
+        data = JSON.parse(dataUser);
+    }
+
+    console.log(data);
+
 }
+
+function logout() {
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = "index.html";
+
+}
+
+// ADD LANÇAMENTO //
+
+document.getElementById("transaction-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const date = document.getElementById("date-input").value;
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;  
+    const type = document.querySelector('input[name="type-input"]:checked').value;
+
+    data.transactions.unshift({
+        date: date, value: value, description: description, type: type
+    });
+
+    saveData(data);
+    e.target.reset();
+    myModal.hide(); 
+
+    alert("Lançamento adicionado com sucesso!");
+
+});
+
+function saveData(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+};
